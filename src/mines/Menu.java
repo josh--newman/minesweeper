0d2f0edd07f14fe6ee2e814c1428d5251729aeb7
Josh Newman <josh.newman4390@gmail.com>
2013-10-21 09:06:50 +1000

    Added a basic menu

---
+++ src/mines/Menu.java
@@ -0,0 +1,16 @@
+package mines;
+
+import javax.swing.*;
+
+public class Menu extends JMenuBar{
+	JMenuBar menuBar;
+	JMenu menu;
+	
+	public Menu() {
+		menuBar = new JMenuBar();
+		menu = new JMenu("The best Menu");
+		menuBar.add(menu);
+	}
+	
+	
+}
