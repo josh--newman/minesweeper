a2f67573a9ef9d09a24aee232a3722b9470cd3f7
Josh Newman <josh.newman4390@gmail.com>
2013-10-22 21:44:12 +1000

    Added action listeners to difficulty menu items

--- src/mines/Menu.java
+++ src/mines/Menu.java
@@ -1,19 +1,23 @@
 package mines;
 
-import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Menu extends JMenuBar{
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class Menu {
 	
 	public static JMenuBar create() {
 		JMenuBar menuBar = new JMenuBar();
 		
 		// Create 'file' menu and items
 		JMenu fileMenu = new JMenu("File");
-		JMenuItem newGame = new JMenuItem("New Game");
-		JMenuItem saveGame = new JMenuItem("Save Game");
-		JMenuItem loadGame = new JMenuItem("Load Game");
-		JMenuItem highScores = new JMenuItem("High Scores");
-		JMenuItem solveGame = new JMenuItem("Solve");
+		JMenuItem newGame = new JMenuItem(new NewGameAction("New Game"));
+		JMenuItem saveGame = new JMenuItem(new SaveGameAction("Save Game"));
+		JMenuItem loadGame = new JMenuItem(new LoadGameAction("Load Game"));
+		JMenuItem highScores = new JMenuItem(new HighScoresAction("High Scores"));
+		JMenuItem solveGame = new JMenuItem(new SolveGameAction("Solve"));
 		
 		// Create 'edit' menu and items
 		JMenu editMenu = new JMenu("Edit");
@@ -22,9 +26,9 @@ public class Menu extends JMenuBar{
 		
 		// Create 'difficulty' menu and items
 		JMenu difficultyMenu = new JMenu("Difficulty");
-		JMenuItem easy = new JMenuItem("Easy");
-		JMenuItem medium = new JMenuItem("Medium");
-		JMenuItem hard = new JMenuItem("Hard");
+		JMenuItem easy = new JMenuItem(new EasyDiffAction("Easy"));
+		JMenuItem medium = new JMenuItem(new MediumDiffAction("Medium"));
+		JMenuItem hard = new JMenuItem(new HardDiffAction("Hard"));
 		
 		// Add FILE items to menu
 		fileMenu.add(newGame);
@@ -49,4 +53,90 @@ public class Menu extends JMenuBar{
 		
 		return menuBar;
 	}
+	
+	
 }
+
+//FILE MENU ACTIONS ***************************
+class NewGameAction extends AbstractAction {
+	public NewGameAction(String text) {
+		super(text);
+	}
+	public void actionPerformed(ActionEvent e) {
+		// call create a new game method here
+		System.out.println("New game item pressed");
+	}
+}
+
+class LoadGameAction extends AbstractAction {
+	public LoadGameAction(String text) {
+		super(text);
+	}
+	public void actionPerformed(ActionEvent e) {
+		// call load game method here
+		System.out.println("Load game item pressed");
+	}
+}
+
+class SaveGameAction extends AbstractAction {
+	public SaveGameAction(String text) {
+		super(text);
+	}
+	public void actionPerformed(ActionEvent e) {
+		// call save game method here
+		System.out.println("Save game item pressed");
+	}
+}
+
+class HighScoresAction extends AbstractAction {
+	public HighScoresAction(String text) {
+		super(text);
+	}
+	public void actionPerformed(ActionEvent e) {
+		// call display high scores window here
+	}
+}
+
+class SolveGameAction extends AbstractAction {
+	public SolveGameAction(String text) {
+		super(text);
+	}
+	public void actionPerformed(ActionEvent e) {
+		// call solve game method here
+	}
+}
+	
+// DIFFICULTY MENU ACTIONS
+class EasyDiffAction extends AbstractAction {
+	public EasyDiffAction(String text) {
+		super(text);
+	}
+	public void actionPerformed(ActionEvent e) {
+		// close current frame
+		// open new game with easy difficulty
+		new Mines("easy");
+	}
+}
+
+class MediumDiffAction extends AbstractAction {
+	public MediumDiffAction(String text) {
+		super(text);
+	}
+	public void actionPerformed(ActionEvent e) {
+		// close current frame
+		
+		// open new game with medium difficulty
+		new Mines("medium");
+	}
+}
+	
+class HardDiffAction extends AbstractAction {
+	public HardDiffAction(String text) {
+		super(text);
+	}
+	public void actionPerformed(ActionEvent e) {
+		// close current frame
+		// open new game with hard difficulty
+		new Mines("hard");
+	}
+}	
